6a699b5b7b60534b384cf6993d559d83bf994f77
Charizard11 <baskicisamet@gmail.com>
2017-11-24 06:01:19 +0300

    displaying all properties of recipe

--- src/main/java/com/sam/spring5recipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/com/sam/spring5recipeapp/bootstrap/RecipeBootstrap.java
@@ -147,6 +147,10 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
         guacRecipe.getCategories().add(americanCategory);
         guacRecipe.getCategories().add(mexicanCategory);
 
+        guacRecipe.setUrl("www.sametbaskici.com");
+        guacRecipe.setServing(4);
+        guacRecipe.setSource("simple recipe");
+
         //add to return list
         recipes.add(guacRecipe);
 
