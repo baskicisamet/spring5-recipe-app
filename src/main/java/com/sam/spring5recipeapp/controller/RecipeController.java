b80a893fbfa4453fdd1516f8591eb493e126cfe0
Charizard11 <baskicisamet@gmail.com>
2017-12-02 08:26:59 +0300

    displaying enum list

--- src/main/java/com/sam/spring5recipeapp/controller/RecipeController.java
+++ src/main/java/com/sam/spring5recipeapp/controller/RecipeController.java
@@ -18,16 +18,16 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @GetMapping
-    @RequestMapping("/recipe/{id}/show/")
+
+    @GetMapping("/recipe/{id}/show/")   // its equal to @RequestMapping("url",method=RequestMethod.GET)
     public String showById(@PathVariable String id, Model model){
 
         model.addAttribute("recipe",recipeService.findById(new Long(id)));
         return "recipe/show";
     }
 
-    @GetMapping
-    @RequestMapping("recipe/new")
+
+    @GetMapping("recipe/new")
     public String newRecipe(Model model){
         model.addAttribute("recipe", new RecipeCommand());
 
@@ -35,23 +35,23 @@ public class RecipeController {
 
     }
 
-    @GetMapping
-    @RequestMapping("recipe/{id}/update")
+
+    @GetMapping("recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
         return  "recipe/recipeform";
     }
 
-    @PostMapping
-    @RequestMapping("recipe")
+
+    @PostMapping("recipe") // // its equal to @RequestMapping("url",method=RequestMethod.POST)
     public String saveOrUpdate(@ModelAttribute RecipeCommand command){
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
         return "redirect:/recipe/"+ savedCommand.getId() +"/show/" ;
     }
 
-    @GetMapping
-    @RequestMapping("recipe/{id}/delete")
+
+    @GetMapping("recipe/{id}/delete")
     public String deleteById(@PathVariable String id){
 
         log.debug("Deleting id: " + id);
