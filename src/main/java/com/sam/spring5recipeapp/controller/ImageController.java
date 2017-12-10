51c05b87e573f39b07980fad08a887380128f092
Charizard11 <baskicisamet@gmail.com>
2017-12-11 01:50:47 +0300

    displaying images from Database

--- src/main/java/com/sam/spring5recipeapp/controller/ImageController.java
+++ src/main/java/com/sam/spring5recipeapp/controller/ImageController.java
@@ -1,7 +1,9 @@
 package com.sam.spring5recipeapp.controller;
 
+import com.sam.spring5recipeapp.commands.RecipeCommand;
 import com.sam.spring5recipeapp.service.ImageService;
 import com.sam.spring5recipeapp.service.RecipeService;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Controller
 public class ImageController {
@@ -29,6 +37,31 @@ public class ImageController {
         model.addAttribute("recipe",recipeService.findCommandById(Long.valueOf(id)));
 
         return "recipe/imageuploadform";
+    }
+
+
+    @GetMapping("recipe/{id}/recipeimage")
+    public void renderImageFromDB(@PathVariable String id, HttpServletResponse response) throws IOException{
+
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(id));
+
+        if(recipeCommand != null){
+
+
+            byte[] byteArray = new byte[recipeCommand.getImage().length];
+            int i= 0;
+
+            for(Byte wrappedByte : recipeCommand.getImage() ){
+
+                byteArray[i++] = wrappedByte;
+            }
+
+            response.setContentType("image/jpeg");
+            InputStream is = new ByteArrayInputStream(byteArray);
+            IOUtils.copy(is,response.getOutputStream());
+
+        }
+
     }
 
 
