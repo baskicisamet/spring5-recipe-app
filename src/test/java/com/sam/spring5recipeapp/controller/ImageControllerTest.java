7303adf1d9d3d226536181f3333e1f256a9d5907
Charizard11 <baskicisamet@gmail.com>
2017-12-10 20:49:10 +0300

    persisting images to database

--- src/test/java/com/sam/spring5recipeapp/controller/ImageControllerTest.java
+++ src/test/java/com/sam/spring5recipeapp/controller/ImageControllerTest.java
@@ -70,7 +70,7 @@ public class ImageControllerTest {
 
         mockMvc.perform(multipart("/recipe/1/image").file(multipartFile))
                 .andExpect(status().is3xxRedirection())
-                .andExpect(header().string("Location", "/recipe/1/show"));
+                .andExpect(header().string("Location", "/recipe/1/show/"));
 
         verify(imageService, times(1)).saveImageFile(anyLong(), any());
     }
