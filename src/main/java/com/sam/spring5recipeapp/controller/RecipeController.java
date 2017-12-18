f1ccd368e44dfd2e793c60b671b3829165a4314c
Charizard11 <baskicisamet@gmail.com>
2017-12-18 14:27:23 +0300

    adding global exception handling

--- src/main/java/com/sam/spring5recipeapp/controller/RecipeController.java
+++ src/main/java/com/sam/spring5recipeapp/controller/RecipeController.java
@@ -75,16 +75,6 @@ public class RecipeController {
         return modelAndView;
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(NumberFormatException.class)
-    public ModelAndView handleNumberFormat(Exception exception){
-        log.error("HandlingNumber Format Exception");
-        log.error(exception.getMessage());
 
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("400error");
-        modelAndView.addObject("exception",exception);
-        return modelAndView;
-    }
 
 }
